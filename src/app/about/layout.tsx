b66c10d10fbad3dfc9fc185dbc3751bb3ef572c3
Donato4Humans <donato.4work@gmail.com>
2025-05-29 00:18:28 +0300

    - part-1 app router base

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'AboutLayout metadata'
+}
+type Props = { children: React.ReactNode }
+const AboutLayout =
+({children}: Props) => {
+    return (
+        <div>
+            <hr/>
+            ABOUT LAYOUT
+            {children}
+            <hr/>
+        </div>
+    )
+};
+
+export default AboutLayout;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <div>
+            About Page content
+        </div>
+    );
+};
+
+export default AboutPage;
