dfcc5a33d4e5f4b537bb67f63dcd045584be4dd4
Donato4Humans <donato.4work@gmail.com>
2025-06-02 21:18:42 +0300

    - part-4(CSR) in lesson3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
 
+    // PART-0
     // TO MAKE OUR PRODUCTION-READY PROJECT(SITE) WE NEED TO RUN IN TERMINAL(npm run build | OR ADD IDE CONFIGURATION) AND APP WILL BE READY TO RUN ON SERVER
     // ALSO TO RUN BUILT VERSION WE NEED TO RUN IN TERMINAL OR IDE CONFIGURATION(npm run start)
+
+    // PART-4
+    //HERE WE USE CSR(CLIENT-SIDE-RENDERING) FOR CLIENT-ORIENTED PAGES OR COMPONENTS(like client-hooks or local-storage(only in useEffect), redux state manager)
+    // we need to add "use client" at the top of page or component to use all-client related staff
+
+    // ALSO WE MUST MAKE CLIENT-SIDE ONLY SPECIFIC COMPONENTS TO AVOID CHAINED CLIENT-SIDE RENDERING OF ALL ABOVE LEVELED COMPONENTS
+
+    // MOSTLY USED FOR ALL HOOKS, FORMS, LOCAL-STORAGE INTERACTIONS
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -55,53 +65,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer/>
     </div>
   );
 }
