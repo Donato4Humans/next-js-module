d4df6cb24237022734e9b2fd791c480458ae9461
Donato4Humans <donato.4work@gmail.com>
2025-06-03 00:18:42 +0300

    - finished hw3

--- src/components/forms/FormComponent.tsx
+++ src/components/forms/FormComponent.tsx
@@ -1,15 +1,39 @@
-import Form from "next/form";
+'use client'
+
 import {saveCar} from "@/server-actions/serverActions";
+import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {carValidator} from "@/validators/carValidator";
+import {ICar} from "@/models/ICar";
 
 const FormComponent = () => {
+
+    const {register, handleSubmit, formState: {errors , isValid}} =
+        useForm<ICar>({mode:"all", resolver:joiResolver(carValidator)});
+
+    const customHandler = (data: ICar) => {
+        saveCar(data);
+    }
+
     return (
         <div>
-            <Form action={saveCar}>
-                <input className="border-2 border-violet-600" type="text" name={'brand'} placeholder={'enter car brand'}/>
-                <input className="border-2 border-violet-600" type="number" name={'price'} placeholder={'enter car price'}/>
-                <input className="border-2 border-violet-600" type="number" name={'year'} placeholder={'enter car year'}/>
-                <button>send</button>
-            </Form>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <div className='border-2 border-violet-600'>
+                    <input type="text" placeholder={'enter car brand'} {...register('brand')}/>
+                    {errors.brand && <div>{errors.brand?.message}</div>}
+                </div>
+
+                <div className='border-2 border-violet-600'>
+                    <input type="number" placeholder={'enter car price'} {...register('price')}/>
+                    {errors.price && <div>{errors.price?.message}</div>}
+                </div>
+
+                <div className='border-2 border-violet-600'>
+                    <input type="number" placeholder={'enter car year'} {...register('year')}/>
+                    {errors.year && <div>{errors.year?.message}</div>}
+                </div>
+                <button className='border-2' disabled={!isValid}>save-car</button>
+            </form>
         </div>
     );
 };
--- src/server-actions/serverActions.tsx
+++ src/server-actions/serverActions.tsx
@@ -3,31 +3,7 @@
 import {ICar} from "@/models/ICar";
 import {carService} from "@/services/api.service";
 
-export const saveCar = async (formData: FormData) => {
-
-    let price: number = 0;
-    let year: number = 0;
-    let newCar: ICar = {brand: '', price: 0, year: 0};
-
-    const brand = formData.get('brand')?.toString();
-    const priceStr = formData.get('price')?.toString();
-    const yearStr = formData.get('year')?.toString();
-
-    if(priceStr && yearStr)
-    {
-        price = parseFloat(priceStr);
-        year = parseInt(yearStr);
-    }
-    if(brand){
-        newCar = {
-            brand: brand,
-            price: price,
-            year: year
-        }
-    }
-
-    console.log(newCar);
-
+export const saveCar = async (newCar: ICar) => {
     await carService.postCar(newCar);
 }
 
