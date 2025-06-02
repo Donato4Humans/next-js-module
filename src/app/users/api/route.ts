7a92597e0a94e4c04834917554a14dbd395dcc0a
Donato4Humans <donato.4work@gmail.com>
2025-06-02 22:08:51 +0300

    - part-5(route-handler) in lesson3

---
+++ src/app/users/api/route.ts
@@ -0,0 +1,29 @@
+
+// localhost:3000/hello/api [GET]
+export const GET = async (request: Request, response: Response) => {
+
+    // MAIN REASON TO USE SUCH SERVER-HANDLER IS TO HIDE SOME SENSITIVE INFO(API-KEYS OR TOKENS) WHILE CALLING API AND SENDING RESPONSE UNDER THE HOOD
+
+    console.log('GET request route handler');
+
+    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json());
+
+    console.log(request);
+    console.log(response);
+
+    // return Response.json({message: request.url, status: 200}); // other way to use server-handler to check url status
+
+    // OTHER PURPOSE IS FOR SIMPLE REDIRECTING BUT IT CAN BE DONE OTHER WAY
+
+    response.headers.set('XXX', 'YYY'); // we can set headers to response  or get from request, etc.
+    return Response.json(users);
+}
+
+// localhost:3000/hello/api [GET]
+export const POST = () => {
+    console.log('POST request route handler');
+    return Response.json({message: 'POST request route handler'});
+}
+
+// ALSO SUCH HANDLER SUPPORT HTTP-METHODS: PUT, PATCH, DELETE, HEAD, OPTIONS
